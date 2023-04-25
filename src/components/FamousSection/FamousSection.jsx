7adcba230f4c757b72c2615c75a11e572673f45a
Keegan Odell <Keegan.Odell@icloud.com>
2023-04-25 16:34:10 -0500

    added fetch

--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -1,45 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './FamousSection.css';
+import axios from 'axios';
 
 function FamousSection() {
-  let [famousPersonName, setPersonName] = useState('');
-  let [famousPersonRole, setPersonRole] = useState('');
-  let [famousPeopleArray, setPeopleArray] = useState([]);
+	let [famousPersonName, setPersonName] = useState('');
+	let [famousPersonRole, setPersonRole] = useState('');
+	let [famousPeopleArray, setPeopleArray] = useState([]);
 
-  // TODO: on load, call the fetchPeople() function
+	// TODO: on load, call the fetchPeople() function
+	useEffect(() => {
+		fetchPeople();
+	}, []);
 
-  const fetchPeople = () => {
-    // TODO: fetch the list of people from the server
-  }
+	const fetchPeople = () => {
+		// TODO: fetch the list of people from the server
+		axios({
+			method: 'GET',
+			url: '/people',
+		})
+			.then((response) => {
+				setPeopleArray(response.data);
+			})
+			.catch((error) => {
+				console.log('gosh darn', error);
+			});
+	};
 
-  const addPerson = (evt) => {
-    evt.preventDefault();
-    console.log(`The person is ${famousPersonName} and they're famous for ${famousPersonRole}`);
-    
-    // TODO: create POST request to add this new person to the database
+	const addPerson = (evt) => {
+		evt.preventDefault();
+		console.log(
+			`The person is ${famousPersonName} and they're famous for ${famousPersonRole}`
+		);
 
-    // HINT: the server is expecting a person object 
-    //       with a `name` and a `role` property
-  
-  }
+		// TODO: create POST request to add this new person to the database
 
-    return (
-      <section className="new-person-section">
-        <form onSubmit={addPerson}>
-          <label htmlFor="name-input">Name:</label>
-          <input id="name-input" onChange={e => setPersonName(e.target.value)} />
-          <label htmlFor="role-input">Famous for:</label>
-          <input id="role-input" onChange={e => setPersonRole(e.target.value)} />
-          <button type="submit">Done</button>
-        </form>
-        <p>
-          {famousPersonName} is famous for "{famousPersonRole}".
-        </p>
-        <ul>
-          {/* TODO: Render the list of famous people */}
-        </ul>
-      </section>
-    );
+		// HINT: the server is expecting a person object
+		//       with a `name` and a `role` property
+	};
+
+	return (
+		<section className='new-person-section'>
+			<form onSubmit={addPerson}>
+				<label htmlFor='name-input'>Name:</label>
+				<input
+					id='name-input'
+					onChange={(e) => setPersonName(e.target.value)}
+				/>
+				<label htmlFor='role-input'>Famous for:</label>
+				<input
+					id='role-input'
+					onChange={(e) => setPersonRole(e.target.value)}
+				/>
+				<button type='submit'>Done</button>
+			</form>
+			<p>
+				{famousPersonName} is famous for "{famousPersonRole}".
+			</p>
+			<ul>
+				{famousPeopleArray.map((person) => {
+					retrun(
+						<li key={person.id}>
+							{person.name} is famous for "{person.role}"
+						</li>
+					);
+				})}
+			</ul>
+		</section>
+	);
 }
 
 export default FamousSection;
