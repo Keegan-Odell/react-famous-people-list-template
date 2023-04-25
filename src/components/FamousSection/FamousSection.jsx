1dc47e24497c4a79dc8dbf5fd10bd285c0401f59
Keegan Odell <Keegan.Odell@icloud.com>
2023-04-25 16:51:30 -0500

    beep boop bop

--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -33,7 +33,22 @@ function FamousSection() {
 		);
 
 		// TODO: create POST request to add this new person to the database
-
+		axios({
+			method: 'POST',
+			url: '/people',
+			data: {
+				name: famousPersonName,
+				role: famousPersonRole,
+			},
+		})
+			.then((response) => {
+				setPersonName('');
+				setPersonRole('');
+				fetchPeople();
+			})
+			.catch((error) => {
+				console.log('gosh darn', error);
+			});
 		// HINT: the server is expecting a person object
 		//       with a `name` and a `role` property
 	};
@@ -44,11 +59,13 @@ function FamousSection() {
 				<label htmlFor='name-input'>Name:</label>
 				<input
 					id='name-input'
+					value={famousPersonName}
 					onChange={(e) => setPersonName(e.target.value)}
 				/>
 				<label htmlFor='role-input'>Famous for:</label>
 				<input
 					id='role-input'
+					value={famousPersonRole}
 					onChange={(e) => setPersonRole(e.target.value)}
 				/>
 				<button type='submit'>Done</button>
@@ -58,7 +75,7 @@ function FamousSection() {
 			</p>
 			<ul>
 				{famousPeopleArray.map((person) => {
-					retrun(
+					return (
 						<li key={person.id}>
 							{person.name} is famous for "{person.role}"
 						</li>
